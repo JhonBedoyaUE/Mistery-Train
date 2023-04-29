710f1b41eaf8135144d5544c22c72ead8f6b703c
JhonBedoyaUE <jhon.bedoya@est.uexternado.edu.co>
2023-04-29 17:52:26 -0500

    new file:   game_modules/game.py 	modified:   main.py

---
+++ game_modules/game.py
@@ -0,0 +1,38 @@
+import pygame
+from game_modules.entities import *
+from game_modules.utilities import *
+import levels.levelExample as template
+
+def start_game(windowSize:tuple[int]=(800,600), FPS:int=60):
+    pygame.init()
+
+    CLOCK = pygame.time.Clock()
+
+    screen = pygame.display.set_mode(windowSize)
+
+    levelWorksSpace = template.create_level(windowSize)
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                running = False
+        if not running:
+            break
+
+        screen.fill((0,0,0))
+
+        keys = pygame.key.get_pressed()
+
+        levelWorksSpace.listen_mouse(pygame.mouse.get_pos(), pygame.mouse.get_pressed(3))
+
+        levelWorksSpace.listen_keys_all(pygame.key.get_pressed())
+
+
+        levelWorksSpace.show(screen)
+
+        levelWorksSpace.update_all(FPS)
+        pygame.display.update()
+        CLOCK.tick(FPS)
--- main.py
+++ main.py
@@ -1,42 +1,11 @@
-import pygame
-import levels.levelExample as template
+import game_modules.game as game
 
-#Importante: Para no saturar el archivo principal los niveles deben crearse como archivos .py en la carpeta /levels
+#Importante: Para no saturar el archivo principal:
+# La administracion del juego debe realizarse en el archivo /game_modules/game.py
+# El juego se inicia llamando la funcion start_game del modulo game_modules.game que representa el archivo /game_modules/game.py 
+# Los niveles deben crearse como archivos .py en la carpeta /levels
 # En el archivo del nivel debe existir una funcion llamada create_level() que retornara el LevelWorsSpace del nivel 
 # Ademas los modulos deben estar en la carpeta /game_modules para mantener en orden los archivos del juego
 # Aún falta textos o menus contextuales
 
-pygame.init()
-
-FPS = 60
-CLOCK = pygame.time.Clock()
-SIZE = (800,600)
-
-screen = pygame.display.set_mode(SIZE)
-
-levelWorksSpace = template.create_level(SIZE)
-
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            running = False
-    if not running:
-        break
-
-    screen.fill((0,0,0))
-
-    keys = pygame.key.get_pressed()
-
-    levelWorksSpace.listen_mouse(pygame.mouse.get_pos(), pygame.mouse.get_pressed(3))
-
-    levelWorksSpace.listen_keys_all(pygame.key.get_pressed())
-
-
-    levelWorksSpace.show(screen)
-
-    levelWorksSpace.update_all(FPS)
-    pygame.display.update()
-    CLOCK.tick(FPS)
+game.start_game()
